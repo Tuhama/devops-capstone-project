79c24d531695d91ed5b3357cd82e88f64ef82aa6
Tuhama <tuhama9@yahoo.com>
2025-08-15 16:05:19 +0300

    fix indent test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -156,7 +156,7 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-  def test_update_account(self):
+    def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
